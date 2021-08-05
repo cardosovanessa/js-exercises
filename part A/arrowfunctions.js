2f5ebe7834097bc900e1eec42052c0d543b24a55
Vanessa Cardoso <cardosovanessa@outlook.com>
2021-08-04 22:35:30 -0300

    s

---
+++ part A/arrowfunctions.js
@@ -0,0 +1,12 @@
+//Transforme as funções declarativas abaixo em Arrow Functions
+const upperName = (name) => {
+  return name.toUpperCase();
+};
+
+const myFunction = (p1, p2) => {
+  return p1 * p2;
+};
+
+const toCelsius = (fahrenheit) => {
+  return (5 / 9) * (fahrenheit - 32);
+};
